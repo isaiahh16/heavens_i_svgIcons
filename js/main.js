54d37f2f81e973545c80991d9c1b3e70884a58c9
Isaiah Heavens <isaiahheavens@Isaiahs-MBP.fc.ca>
2023-01-13 11:16:47 -0500

    adding images and JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log ('Javascript is running');
+
+/* longer JS block comment
+
+const is a type of variable - a bit of memory that can hold a value
+
+JS uses (can use) ANY valid css selector to make a connection to ANY element on the page - it works the same way as CSS selectors do, because its USING css selectors to fid matching element(s)
+
+the document is the DOM - the web page and all of its elements
+
+querySelector is the method (funtion) that makes the connection between JS and the DOM (the elements)
+*/
+
+//step 1 - make the connection to the element(s) you want to interact with
+const jpgGraphic = document.querySelector('#bitmap');
+// creating vector click event (steps 1, 2 and 3) DONT need a second function
+const svgGraphic = document.querySelector("#vector");
+
+// step 3  - what funtion do you want to run
+    function logID() {
+    console.log('clicked on this element:',this.id);
+}
+
+//step 2 - decide how you want the user to interact with the object you created in step 1
+jpgGraphic.addEventListener('click', logID);
+svgGraphic.addEventListener('click', logID);
+
+
+
+
